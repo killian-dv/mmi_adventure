437ee29978882f8b5f3327ce0b2ff780b42d588d
Benjamin Frenal <benjamin.frenal@etudiant.univ-reims.fr>
2022-06-13 16:32:25 +0200

    modification by benji

--- register.php
+++ register.php
@@ -3,12 +3,16 @@
     <section class="background">
     <?php require('inc/header.php'); ?>
 
-    <h1 class="m15">Inscription</h1>
+    <h1 class="m17">Inscription</h1>
 
     <div id="register">
         <label for="ekip_name">Renseignez le nom de votre équipe si dessous :</label></br>
         <input type="text" id="ekip_name" name="ekip_name" minlength="3" size="10">
     </div>
+
+    <form action="register.php" method="POST">
+        <input type="submit" value="Submit">
+    </form> 
 
     </section>
     <?php require('inc/footer.php'); ?>
