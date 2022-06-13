15f7e9b6ed98c49f8987f39749ee227bc6dd421e
Benjamin Frenal <benjamin.frenal@etudiant.univ-reims.fr>
2022-06-13 16:02:58 +0200

    modification by benji

--- register.php
+++ register.php
@@ -1,5 +1,6 @@
     <?php require('inc/debut_html.php'); ?>
     <title>MMI Adventure - Inscription</title>
+    <section class="background">
     <?php require('inc/header.php'); ?>
 
     <h1>Inscription</h1>
@@ -7,5 +8,7 @@
     <label for="register">Renseignez le nom de votre équipe si dessous :</label>
     <input type="text" id="register" name="register" minlength="3" size="10">
 
+    </section>
+    <?php require('inc/footer.php'); ?>
 </body>
 </html>
