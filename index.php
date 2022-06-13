3bc9493bfff668a8e20262a0f29512a9e4787f3a
Killian David <killian@MacBook-Air-de-Killian.local>
2022-06-13 15:58:57 +0200

    modification de killix via GitHub

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
     <?php require('inc/debut_html.php'); ?>
     <title>MMI Adventure - Accueil</title>
+    <section class="background">
     <?php require('inc/header.php'); ?>
+    </section>
+
+
+
+    <?php require('inc/footer.php'); ?>
 </body>
 </html>
