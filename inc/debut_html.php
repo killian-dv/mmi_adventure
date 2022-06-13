28b45fd45b21e4b5fb829c36d86cb4e30894a51c
Killian David <killian@MacBook-Air-de-Killian.local>
2022-06-13 12:12:43 +0200

    modification de killix via GitHub

--- inc/debut_html.php
+++ inc/debut_html.php
@@ -5,4 +5,4 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css" />
-    <link rel="icon" type="image/png" href="logo.png" />
+    <link rel="icon" type="image/png" href="img/logo_mmi_adventure.png" />
