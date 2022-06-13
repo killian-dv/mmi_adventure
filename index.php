eb0b1a6dc8101c8df82760786c4bede5058d558c
Killian David <killian@MacBook-Air-de-Killian.local>
2022-06-13 16:14:30 +0200

    modification de killix via GitHub

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
     <title>MMI Adventure - Accueil</title>
     <section class="background">
     <?php require('inc/header.php'); ?>
+    <img class="plage_blender" src="img/plage_blender.png" alt="plage en 3D">
+
+    <h1>En quoi consiste le jeu ?</h1>
+    <h1>Consignes :</h1>
     </section>
-
-
-
     <?php require('inc/footer.php'); ?>
 </body>
 </html>
