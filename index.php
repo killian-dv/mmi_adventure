7c2d736378bb45a6a9d5396d3f5218418ce2981a
Benjamin Frenal <benjamin.frenal@etudiant.univ-reims.fr>
2022-06-13 12:11:36 +0200

    modification by benji

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
     <?php require('inc/debut_html.php'); ?>
     <title>MMI Adventure</title>
     <?php require('inc/header.php'); ?>
-    <p>Bienvenue sur MMI Adventure</p>
-</head>
-<body>
     <p>Bienvenue sur MMI Adventure. Blalala</p>
 </body>
 </html>
