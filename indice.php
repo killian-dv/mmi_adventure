5da409316a498f8df320c458dd093b2cc41e8359
Killian David <killian@MacBook-Air-de-Killian.local>
2022-06-14 08:04:38 +0200

    modification de killix via GitHub

--- indice.php
+++ indice.php
@@ -1,5 +1,6 @@
 <?php require('inc/debut_html.php'); ?>
     <title>MMI Adventure - Indice</title>
+    <section class="background">
     <?php require('inc/header.php'); ?>
     <?php
         if(!empty($_GET['id'])){
@@ -9,8 +10,15 @@
             if ($_GET['id'] == "3") { echo "<h1>Indice 3</h1>".$date;}
         } else{
             // page indice sans ?id
-            echo "<h1>Vous êtes sur la page Indice, des QrCodes sont cachés un peu partout dans l'IUT (pas d'id selectionné)</h1>";
+            //echo "<h1>Vous êtes sur la page Indice, des QrCodes sont cachés un peu partout dans l'IUT (pas d'id selectionné)</h1>";
         }
     ?>
+    <h1 class="title_numéro_indice">Indice n°01</h1>
+    <h1>Anecdote</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse dolores ab eaque aut accusantium optio, laudantium nostrum similique minima possimus.</p>
+
+    <p>Total des indices trouvés : XX sur XX</p>
+    </section>
+    <?php require('inc/footer.php'); ?>
 </body>
 </html>
