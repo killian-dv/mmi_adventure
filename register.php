f269ecdfb059bbb1c0975fe56372de64a043cd34
Benjamin Frenal <benjamin.frenal@etudiant.univ-reims.fr>
2022-06-14 16:01:05 +0200

    modification by benji

--- register.php
+++ register.php
@@ -9,14 +9,10 @@
             $ekip_name = $_POST['ekip_name'];
             $date = date("H:i:s");
             $mabd = connexion_mmi_adventure();
-            if (doublon($mabd, $ekip_name)==false){
                 ajout_equipe($mabd, $ekip_name, $date);
-                setcookie("id_ekip", recup_id_ekip($mabd, $ekip_name), time()+3600);
+                setcookie("id_ekip", recup_id_ekip($mabd, $ekip_name), time()+3600, '/');
                 echo '<div class="quit_page"><p>Vous pouvez quitter cette page et préparer votre appareil photo !</br></br>Bonne chance '.$ekip_name.' !</p></div>';
                 echo $_COOKIE["id_ekip"];
-            }else {
-                header('location: register.php');
-            }
             deconnexion_mmi_adventure($mabd);
         } else{ 
             echo'
