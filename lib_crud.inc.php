4f1185250c27ff3bb21a318bc7c325df77a2e085
Benjamin Frenal <benjamin.frenal@etudiant.univ-reims.fr>
2022-06-15 15:33:45 +0200

    modification by benji

--- lib_crud.inc.php
+++ lib_crud.inc.php
@@ -398,7 +398,7 @@ $req = 'SELECT total_indice FROM db_adventure WHERE equipe_id ='.$ekip_id.'';
 }
 
 function date_fin($mabd, $ekip_id, $date_fin) {
-  $req = 'UPDATE db_adventure SET heure_fin = '.$date_fin.' WHERE equipe_id ='.$ekip_id.'';
+  $req = 'UPDATE db_adventure SET heure_fin = "'.$date_fin.'" WHERE equipe_id ='.$ekip_id.'';
   echo $req;
   try {
     $resultat = $mabd->query($req);
