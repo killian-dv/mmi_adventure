7311565d35a65e7b46b33308ecd204d705789344
Benjamin Frenal <benjamin.frenal@etudiant.univ-reims.fr>
2022-06-20 16:02:56 +0200

    modification by benji

---
+++ carte.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carte de l'IUT</title>
+</head>
+<body>
+    <img src="img/planiutkiki.svg" alt="Image de la Carte">
+</body>
+</html>
