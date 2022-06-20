f05d58900f4840cd4305f9dae76fb6e0625f6695
Benjamin Frenal <benjamin.frenal@etudiant.univ-reims.fr>
2022-06-20 11:10:45 +0200

    modification by benji

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
     <img class="plage_blender" src="img/plage_blender.png" alt="plage en 3D">
     <div class="paragraphe">
         <h1>En quoi consiste le jeu ?</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere nisi sit, pariatur consequuntur eveniet adipisci debitis sint assumenda nobis tempore laborum, incidunt, omnis aliquid labore veniam impedit saepe. Ex doloribus cumque ea! Aliquid quis adipisci vitae deserunt. Adipisci nesciunt molestiae laudantium facilis, eligendi unde corrupti aliquam, sint deserunt voluptas quos.</p>
+        <p>MMI adventure est une course d'orientation dans laquelle les étudiants devront partir à la recherche de QR codes cachés par groupe de 5. Il faudra scanner tout les QR codes en un minimum de temps.</p>
     </div>
     <div class="paragraphe">
         <h1>Consignes :</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni libero eos ullam reiciendis enim dolor autem voluptates est odio nam possimus, neque perspiciatis delectus architecto quaerat dicta distinctio cum! Similique eum expedita veniam voluptatibus ullam error excepturi laborum, accusamus cum minus, a, laudantium quae rem optio ipsam ut esse quam?</p>
+        <p>Inscrivez-vous avec votre téléphone via notre formulaire (1 par groupe). Les autres membres du groupe peuvent utiliser leur téléphone pour avoir accès à la carte. 
+Dès que vous trouvez un QR code, scannez le avec votre téléphone. Vous verrez apparaitre une anecdote sur le lieu. Tous les 5 QR codes, vous devrez vous rendre à un stand pour résoudre une énigme.</p>
     </div>
     <div class="container_button">
         <a class="inscription_button" href="register.php">Inscription</a>
